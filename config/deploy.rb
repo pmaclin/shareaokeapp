15967814909ae19922a24cbcbcd1c3840d666dc1
Maclin Phil <philmaclin@gmail.com>
2016-06-01 15:55:59 -0500

    Protects yml files
    Changes to example files…

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, '/home/deploy/shareaokeapp'
 # Default value for :pty is false
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for :linked_files is []
