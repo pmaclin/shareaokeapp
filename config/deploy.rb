3a3cbd3ba2433067b00135904c1490d5bb961a31
Maclin Phil <philmaclin@gmail.com>
2016-06-06 18:25:40 -0500

    edits deploy.rb
    Adds set :rbenv_ruby, ‘2.3.1’ line

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.1'
 
+set :rbenv_ruby, '2.3.1'
+
 set :application, 'shareaokeapp'
 set :repo_url, 'https://github.com/pmaclin/shareaokeapp.git'
 
